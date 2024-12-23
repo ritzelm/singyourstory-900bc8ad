94b7a062e5baaf0c6eb21659cb14dfb304febcb2
ritzelm <128750822+ritzelm@users.noreply.github.com>
2024-12-23 20:12:06 +0100

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -105,7 +105,7 @@ const About = () => {
           </div>
           <AudioCard 
             title="Hey Samu, los wir räumen auf" 
-            audioUrl="https://meinkinderlied.de/songs/heysamuloswirräumenauf.mp3" 
+            audioUrl="https://ritzelmut.de/meinkinderlied-songs/annaräumtauf.mp3" 
           />
         </div>
       </section>
