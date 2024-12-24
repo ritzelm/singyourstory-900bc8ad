6eef799f37fd67492620646f36b4eef5e0076034
ritzelm <128750822+ritzelm@users.noreply.github.com>
2024-12-24 12:02:36 +0100

    Update index.ts

--- supabase/functions/create-order/index.ts
+++ supabase/functions/create-order/index.ts
@@ -81,7 +81,7 @@ serve(async (req) => {
       },
       body: JSON.stringify({
         from: "MeinKinderlied <order@meinkinderlied.de>",
-        to: ["flo@ritzelmut.de"],
+        to: ["florian@precisionx.com"],
         subject: `Neue Bestellung: ${orderId}`,
         html: emailHtml,
       }),
