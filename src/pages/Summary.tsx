d59ff518c7346e072b6970519f9cfe2e2ad1066e
ritzelm <128750822+ritzelm@users.noreply.github.com>
2024-12-25 22:50:47 +0100

    Update Summary.tsx

--- src/pages/Summary.tsx
+++ src/pages/Summary.tsx
@@ -24,6 +24,7 @@ const occasionLabels: Record<string, string> = {
   zahneputzen: "Zähneputzen",
   aufraumen: "Aufräumen",
   geburtstag: "Geburtstag",
+  anziehen: "Anziehen",
 };
 
 const genreLabels: Record<string, string> = {
